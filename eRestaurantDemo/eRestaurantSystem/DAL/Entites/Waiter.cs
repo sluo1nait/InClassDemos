486084ead6bfbb7dd9170c5fedba21d5ce22027e
sluo1nait <sluo1@studentmail.nait.ca>
2015-09-30 20:25:38 -0600

    Add remaining entities to project

---
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entites/Waiter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion 
+
+namespace eRestaurantSystem.DAL.Entites
+{
+    public class Waiter
+    {
+        public int WaiterID { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(25)]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(35)]
+        public string LastName { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(15, MinimumLength = 4)]
+        public string Phone { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(30, MinimumLength = 8)]
+        public string Address { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        [NotMapped] //like read only property, not really in DB, you can use it
+        public string FullName { get { return FirstName + " " + LastName; } }
+
+        // Navigation Properties
+        public virtual ICollection<Bill> Bills { get; set; }
+    }
+}
