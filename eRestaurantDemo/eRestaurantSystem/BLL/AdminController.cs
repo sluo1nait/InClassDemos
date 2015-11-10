980df0501423e8b6b1c4ccaf0bf606c64c80e0dc
sluo1nait <sluo1@studentmail.nait.ca>
2015-11-09 21:22:06 -0700

    FrontDesk 4 steps query

--- eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -11,7 +11,7 @@ using eRestaurantSystem.DAL.DTOs;
 using eRestaurantSystem.DAL.POCOs;
 //using System.ComponentModel.DataAnnotations;//used for entity
 using System.ComponentModel;//Object Data Source
-using System.Data.Entity;
+using System.Data.Entity; //help with DateTime and TimeSpan linq concerns
 #endregion 
 
 namespace eRestaurantSystem.BLL
@@ -365,9 +365,125 @@ namespace eRestaurantSystem.BLL
                    return finalResult.OrderBy(x => x.Hour).ToList(); //method syntax
                }
            }
-       
 
 
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SeatingSummary> SeatingByDateTime(DateTime date, TimeSpan newtime)
+
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                var step1 = from data in context.Tables
+                            select new
+                            {
+                                Table = data.TableNumber,
+                                Seating = data.Capacity,
+                                // This sub-query gets the bills for walk-in customers
+                                WalkIns = from walkIn in data.Bills
+                                          where
+                                                 walkIn.BillDate.Year == date.Year
+                                              && walkIn.BillDate.Month == date.Month
+                                              && walkIn.BillDate.Day == date.Day
+                                              //Linq to Entity does not play nicely with DateTime/Timespan
+                                              //of which TimeOfDay belongs
+                                              //&& walkIn.BillDate.TimeOfDay <= newtime
+                                             && DbFunctions.CreateTime(walkIn.BillDate.Hour,walkIn.BillDate.Minute,walkIn.BillDate.Second) <=newtime
+                                              //inside System.Data.Entity is a class of functions
+                                              //that will help with DateTime/TimeSpan concerns
+                                              && (!walkIn.OrderPaid.HasValue || walkIn.OrderPaid.Value >= newtime)
+                                          //                          && (!walkIn.PaidStatus || walkIn.OrderPaid >= time)
+                                          select walkIn,
+                                // This sub-query gets the bills for reservations
+                                //Linq connects to SQL, we don't create the reservationtables entity
+                                //OnModelCreate such table
+                                Reservations = from booking in data.Reservations
+                                               from reservationParty in booking.Bills //dont need Reservation table, 
+                                               //b/c we use the OnModelCreate
+                                               where
+                                                      reservationParty.BillDate.Year == date.Year
+                                                   && reservationParty.BillDate.Month == date.Month
+                                                   && reservationParty.BillDate.Day == date.Day
+                                                   //&& reservationParty.BillDate.TimeOfDay <= newtime
+                                                   && DbFunctions.CreateTime(reservationParty.BillDate.Hour, reservationParty.BillDate.Minute, reservationParty.BillDate.Second) <= newtime
+                                                   && (!reservationParty.OrderPaid.HasValue || reservationParty.OrderPaid.Value >= newtime)
+                                               //                          && (!reservationParty.PaidStatus || reservationParty.OrderPaid >= time)
+                                               select reservationParty
+                            };
+              
+
+
+                var step2 = from data in step1.ToList() // .ToList() forces the first result set to be in memory
+                            select new
+                            {
+                                Table = data.Table,
+                                Seating = data.Seating,
+                                CommonBilling = from info in data.WalkIns.Union(data.Reservations)
+                                                select new // info
+                                                {
+                                                    BillID = info.BillID,
+                                                    BillTotal = info.Items.Sum(bi => bi.Quantity * bi.SalePrice),
+                                                    Waiter = info.Waiter.FirstName,
+                                                    Reservation = info.Reservation
+                                                }
+                            };
+                //we call it Items, so we change from BillItems to Items above
+               
+
+
+                // Step 3 - Get just the first CommonBilling item
+                //         (presumes no overlaps can occur - i.e., two groups at the same table at the same time)
+                var step3 = from data in step2.ToList()
+                            select new
+                            {
+                                Table = data.Table,
+                                Seating = data.Seating,
+                                Taken = data.CommonBilling.Count() > 0,
+                                // .FirstOrDefault() is effectively "flattening" my collection of 1 item into a 
+                                // single object whose properties I can get in step 4 using the dot (.) operator
+                                CommonBilling = data.CommonBilling.FirstOrDefault()
+                            };
+                
+
+                // Step 4 - Build our intended seating summary info
+                var step4 = from data in step3
+                            select new  SeatingSummary() // the DTO class to use in my BLL
+                            {
+                                Table = data.Table,
+                                Seating = data.Seating,
+                                Taken = data.Taken,
+                                // use a ternary expression to conditionally get the bill id (if it exists)
+                                BillID = data.Taken ?               // if(data.Taken)
+                                         data.CommonBilling.BillID  // value to use if true
+                                       : (int?)null,               // value to use if false
+                                BillTotal = data.Taken ?
+                                            data.CommonBilling.BillTotal : (decimal?)null,
+                                Waiter = data.Taken ? data.CommonBilling.Waiter : (string)null,
+                                ReservationName = data.Taken ?
+                                                  (data.CommonBilling.Reservation != null ?
+                                                   data.CommonBilling.Reservation.CustomerName : (string)null)
+                                                : (string)null
+                            };
+
+                return step4.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<WaiterOnDuty> ListWaiter()
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                var result = from person in context.Waiters
+                             where person.ReleaseDate == null
+                             select new WaiterOnDuty()
+                             {
+                                 WaiterID = person.WaiterID,
+                                 FullName = person.FirstName + " " + person.LastName
+                             };
+                return result.ToList();
+            }
+        }
   #endregion
-}
+}//eof class
 }//eof namespace
+
---
+++ eRestaurantDemo/eRestaurantSystem/DAL/POCOs/WaiterOnDuty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.DAL.POCOs
+{
+    public class WaiterOnDuty
+    {
+        public int WaiterID { get; set; }
+        public string FullName { get; set; }
+
+    }
+}
