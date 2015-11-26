c1f6bb4e8e03ba8e8496bd9dc3f347ee9f87ce72
sluo1nait <sluo1@studentmail.nait.ca>
2015-11-25 20:09:35 -0700

    Set up security basis
    Security

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/Security/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using eRestaurantSystem.DAL.Entites.Security;
+using eRestaurantSystem.DAL.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.BLL.Security
+{
+       public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
+
+ 
